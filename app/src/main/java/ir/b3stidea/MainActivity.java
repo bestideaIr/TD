291aba99795f25132dce9efc409ce9d1630cddbf
kamran7 <farahvashi.h@gmail.com>
2020-07-29 08:14:29 +0430

    شروع برنامه تیر 1399

---
+++ app/src/main/java/ir/b3stidea/MainActivity.java
@@ -0,0 +1,14 @@
+package ir.b3stidea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
